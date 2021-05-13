b3d938ed43d00f3430c273e9ffbdde49c3cbbf65
njcalvo <njcalvo@gmail.com>
2021-05-13 18:28:53 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { paginate } from './paginate';
 export { IPaginationOptions } from './interfaces/options.interface';
 export { Page } from './interfaces/page.interface';
+console.log('test')
